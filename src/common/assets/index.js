0a3fedba76c11310c7d049a597200088b482f8e6
greencoffee1635 <166354@naver.com>
2022-08-03 20:43:12 +0900

    feat: assets에 index 생성

---
+++ src/common/assets/index.js
@@ -0,0 +1,17 @@
+import toolbarBoundingBox from "./img/Bounding_Box_Create.svg";
+import clearIcon from "./img/clear-24px.svg";
+import minimizeIcon from "./img/minimize.svg";
+import minimizeSmallIcon from "./img/minimize-24px.svg";
+import quitIcon from "./img/Quit.svg";
+import smallBoxes from "./img/smaller.svg";
+import toolbarPointer from "./img/Toolbar_select.svg";
+
+export const baseImg = {
+  toolbarBoundingBox,
+  clearIcon,
+  minimizeIcon,
+  minimizeSmallIcon,
+  quitIcon,
+  smallBoxes,
+  toolbarPointer,
+};
