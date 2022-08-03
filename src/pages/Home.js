624ee36abdaf582da39fa6d5d15b041f4fe62b96
greencoffee1635 <166354@naver.com>
2022-08-03 20:32:11 +0900

    remove: 불필요한 파일 삭제

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ImageData from "../common/components/ImageData";
+
+const Home = () => {
+  return (
+    <div>
+      <ImageData />
+    </div>
+  );
+};
+
+export default Home;
